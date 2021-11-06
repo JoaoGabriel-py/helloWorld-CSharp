50ba13493cf21bd2903734750df651ff07c1b185
JoaoGabriel-py <joao.g.castro20@gmail.com>
2021-11-06 09:53:03 -0300

    Aula de concatenação - ShiftCSharp

---
+++ helloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFilme, sala, hora, data;
+
+            Console.WriteLine("Insira o nome do filme:");
+            nomeFilme = Console.ReadLine();
+
+            Console.WriteLine("Insira a sala:");
+            sala = Console.ReadLine();
+
+            Console.WriteLine("Insira o horário do filme:");
+            hora = Console.ReadLine();
+
+            Console.WriteLine("Data:");
+            data = Console.ReadLine();
+
+            Console.WriteLine($"Seu ingresso para o filme {nomeFilme}, sala {sala} no dia {data} às {hora}.");
+
+        }
+    }
+}
